76251f8fee594bf3dc00f0da81f1916cb4d01128
Alexandr Burakov <bursasha1201@gmail.com>
2022-07-19 22:57:00 +0200

    Adding the second block with slider and text

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
 $(function()
 {
-
+	$('.slider__inner').slick({
+		nextArrow: '<button type="button" class="slick-button slick-next"></button>',
+		prevArrow: '<button type="button" class="slick-button slick-prev"></button>',
+		infinite: false
+	});
 });
