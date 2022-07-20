9daa697212c22cda0443bff4c6cedcc0245f77d9
Alexandr Burakov <bursasha1201@gmail.com>
2022-07-20 18:29:36 +0200

    Adding 7th section News

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function()
 {
-	$('.slider__inner').slick({
+	$('.slider__inner, .news__slider-inner').slick({
 		nextArrow: '<button type="button" class="slick-button slick-next"></button>',
 		prevArrow: '<button type="button" class="slick-button slick-prev"></button>',
 		infinite: false
